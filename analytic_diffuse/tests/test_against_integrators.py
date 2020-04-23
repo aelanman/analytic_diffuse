ddd5d19ff7b0c90bf49f3dfdb8bad797c852b6aa
Steven Murray <steven.murray@curtin.edu.au>
2020-04-23 15:41:24 -0700

    Ensure tests are for ua < 1

--- analytic_diffuse/tests/test_against_integrators.py
+++ analytic_diffuse/tests/test_against_integrators.py
@@ -22,11 +22,14 @@ def test_against_hankel(model, kwargs):
     assert np.allclose(anl, num, rtol=1e-8)
 
 
-@pytest.mark.parametrize('a', (0.1, 0.25, 0.5))  # 0.2 doesn't seem to work for u=10, 100 (numerical or analytic)
+@pytest.mark.parametrize('a', (0.1, 0.2, 0.25, 0.5))  # 0.2 doesn't seem to work for u=10, 100 (numerical or analytic)
 def test_projgauss_mid_a(a):
-    u = np.array([0.1, 1, 10, 100])
+    if a < 0.25:
+        u = np.array([0.01, 0.1, 1]) / a
+    else:
+        u = np.array([0.01, 0.1, 1, 10, 100]) / a
 
-    anl = get_solution('projgauss')(u, a=a, order=1000, chunk_order=20, uselarge=True)
+    anl = get_solution('projgauss')(u, a=a, order=1000, chunk_order=20)
     num = integrators.hankel_solver('projgauss', u, quad_kwargs=dict(epsabs=0, limit=1000, epsrel=1e-8), use_points=True, a=a)
 
     assert np.allclose(anl, num, rtol=1e-8)
